d2d2aed1431a8e061dc31a009ad0c136b130ad0a
Arjan <arjan.neelis@gmail.com>
2023-09-26 17:16:39 +0200

    Updated the code with suctioncup on/off

--- Magnets.py
+++ Magnets.py
@@ -2,15 +2,15 @@ from DRCF import *
 
 # Code for picking&placing magnets
 # Settings for suctioncup
-suction_cup = 1                              # Vacuumpump connected to digital_output 1
-suck_time = 1                                # Number of seconds to wait after (de)activating the vacuumpump
+suction_cup = 1     # Vacuumpump connected to digital_output 1
+suck_time = 1       # Number of seconds to wait after (de)activating the vacuumpump
 # Coordinates for pick and place
-approach_magnet_1 = posx(100, -400, 350, 30, -180, 115)  # Above magnet storage
-approach_magnet_2 = posx(100, -500, 350, 30, -180, 115)  # Above magnet place 1
-approach_magnet_3 = posx(100, -590, 350, 30, -180, 115)  # Above magnet place 2
-pick_magnet = posx(100, -400, 60, 30, -180, 115)        # Pick up position
-place_position_1 = posx(100, -500, 280, 30, -180, 115)     # Place position 1st magnet
-place_position_2 = posx(100, -590, 280, 30, -180, 115)     # Place position 2nd magnet
+approach_magnet_1 = posx(100, -400, 350, 30, -180, 115)     # Above magnet storage
+approach_magnet_2 = posx(100, -500, 350, 30, -180, 115)     # Above magnet place 1
+approach_magnet_3 = posx(100, -590, 350, 30, -180, 115)     # Above magnet place 2
+pick_magnet = posx(100, -400, 60, 30, -180, 115)            # Pick up position
+place_position_1 = posx(100, -500, 280, 30, -180, 115)      # Place position 1st magnet
+place_position_2 = posx(100, -590, 280, 30, -180, 115)      # Place position 2nd magnet
 
 
 def place_magnet_1():
--- Trapezium.py
+++ Trapezium.py
@@ -1,3 +1,24 @@
-#from DRCF import *
+from DRCF import *
 
-#Test
+# Code for picking&placing trapezium
+# Settings for suctioncup
+suction_cup = 1                              # Vacuumpump connected to digital_output 1
+suck_time = 1                                # Number of seconds to wait after (de)activating the vacuumpump
+# Coordinates for pick and place
+approach_trapezium_1 = posx(100, -400, 350, 30, -180, 115)  # Above trapezium storage
+approach_trapezium_2 = posx(50, -400, 110, 75, -180, 115)   # Go up at an angle
+approach_trapezium_3 = posx(107.5, -545, 350, 30, -180, 115)  # Above trapezium place
+pick_trapezium = posx(100, -400, 60, 75, -180, 115)        # Pick up position
+place_trapezium = posx(100, -545, 280, 30, -180, 115)     # Place position 1st magnet
+
+movel(approach_trapezium_1)
+movel(approach_trapezium_2)
+movel(pick_trapezium)
+set_digital_output(suction_cup, ON)  # Activate suctioncup
+wait(suck_time)
+movel(approach_trapezium_2)
+movel(approach_trapezium_1)
+movel(approach_trapezium_3)
+movel(place_trapezium)
+set_digital_output(suction_cup, OFF)  # Activate suctioncup
+wait(suck_time)
