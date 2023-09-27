0989f2bde2b56a1dbf227e0e3c13eedef0b561eb
Arjan <arjan.neelis@gmail.com>
2023-09-27 11:13:54 +0200

    Updated the coordinates and added angled pick-up after testing

--- Magnets.py
+++ Magnets.py
@@ -6,39 +6,44 @@ suction_cup = 1     # Vacuumpump connected to digital_output 1
 suck_time = 1       # Number of seconds to wait after (de)activating the vacuumpump
 # Coordinates for pick and place
 approach_magnet_1 = posx(100, -400, 350, 30, -180, 115)     # Above magnet storage
-approach_magnet_2 = posx(100, -500, 350, 30, -180, 115)     # Above magnet place 1
-approach_magnet_3 = posx(100, -590, 350, 30, -180, 115)     # Above magnet place 2
-pick_magnet = posx(100, -400, 60, 30, -180, 115)            # Pick up position
+approach_magnet_2 = posx(100, -400, 100, 30, -180, 115)
+approach_magnet_3 = posx(100, -500, 350, 30, -180, 115)     # Above magnet place 1
+approach_magnet_4 = posx(100, -590, 350, 30, -180, 115)     # Above magnet place 2
+pick_magnet = posx(140, -400, 60, 30, -180, 115)            # Pick up position
 place_position_1 = posx(100, -500, 280, 30, -180, 115)      # Place position 1st magnet
 place_position_2 = posx(100, -590, 280, 30, -180, 115)      # Place position 2nd magnet
 
 
 def place_magnet_1():
     movel(approach_magnet_1, v=100, a=100)
+    movel(approach_magnet_2, v=100, a=100)
     movel(pick_magnet, v=100, a=100)
     set_digital_output(suction_cup, ON)                   # Activate suctioncup
     wait(suck_time)
-    movel(approach_magnet_1, v=100, a=100)
     movel(approach_magnet_2, v=100, a=100)
+    movel(approach_magnet_1, v=100, a=100)
+    movel(approach_magnet_3, v=100, a=100)
     movel(place_magnet_1, v=100, a=100)
     set_digital_output(suction_cup, OFF)                  # Deactivate suctioncup
     wait(suck_time)
-    movel(approach_magnet_2, v=100, a=100)
+    movel(approach_magnet_3, v=100, a=100)
 
 
 def place_magnet_2():
     movel(approach_magnet_1, v=100, a=100)
+    movel(approach_magnet_2, v=100, a=100)
     movel(pick_magnet, v=100, a=100)
     set_digital_output(suction_cup, ON)                   # Activate suctioncup
     wait(suck_time)
+    movel(approach_magnet_2, v=100, a=100)
     movel(approach_magnet_1, v=100, a=100)
-    movel(approach_magnet_3, v=100, a=100)
+    movel(approach_magnet_4, v=100, a=100)
     movel(place_magnet_2, v=100, a=100)
     set_digital_output(suction_cup, OFF)                  # Deactivate suctioncup
     wait(suck_time)
-    movel(approach_magnet_3, v=100, a=100)
+    movel(approach_magnet_4, v=100, a=100)
 
 
-if get_digital_input(1) == 1:
+if get_digital_input(1) == 1:       # Wait for a signal to start placing magnets
     place_magnet_1()
     place_magnet_2()
